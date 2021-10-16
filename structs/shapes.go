f8c5a2cea93e7e7173d7b1502f707960b5e8dbd0
panvalkar1994 <vpanvalkar@outlook.com>
2021-10-16 13:50:05 +0530

    Shape interface added

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,6 +2,9 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -15,10 +18,10 @@ func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func (r *Rectangle) Area() float64 {
+func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
