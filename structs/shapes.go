cc2d958d95647e58eebe9f4d3369ff35b143f7e3
panvalkar1994 <vpanvalkar@outlook.com>
2021-10-16 14:07:23 +0530

    triangle struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -14,6 +14,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
@@ -24,4 +29,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (tri Triangle) Area() float64 {
+	return 0.5 * (tri.Base * tri.Height)
 }
